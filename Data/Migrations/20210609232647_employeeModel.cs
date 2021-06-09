8c6eb38f9f24cb0a2cbee5ed5e088df70f2033c4
Bart <bart.rybak@hotmail.com>
2021-06-10 00:27:28 +0100

    Add project files.

---
+++ Data/Migrations/20210609232647_employeeModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VacationTracker.Data.Migrations
+{
+    public partial class employeeModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VacationTracker.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Firstname { get; set; }
+        public int Surname { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string JobTitle { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+        // Display DateTime as date only using DisplayFormat attribute
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        // Properties for diplay purposes, not migrated to the database
+        [NotMapped]
+        public string DisplayStartDate { get { return this.StartDate.ToString("dd/MM/yyyy"); } }
+
+        // Password property to store Employee password, not migrated to the database
+        [NotMapped]
+        public string Password { get; set; }
+
+        // Roles
+        public bool IsApprover { get; set; }
+        public bool IsManager { get; set; }
+        public bool IsAdmin { get; set; }
+
+        // Working days
+        public bool Mon { get; set; }
+        public bool Tue { get; set; }
+        public bool Wed { get; set; }
+        public bool Thu { get; set; }
+        public bool Fri { get; set; }
+        public bool Sat { get; set; }
+        public bool Sun { get; set; }
+
+        
+    }
+}
