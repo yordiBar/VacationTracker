a3a98d66e4b292d0858e4bbb205ff6c1dac631cc
Bart <bart.rybak@hotmail.com>
2021-09-10 15:41:07 +0100

    added MyRequests Action and view to display requests for signed in user

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,6 +27,11 @@ namespace VacationTracker.Controllers
             return View();
         }
 
+        public IActionResult MyRequests()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
