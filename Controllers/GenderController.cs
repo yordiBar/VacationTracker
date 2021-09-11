b0df24113c41495d4b8a704302cca9ccda0253a7
Bart <bart.rybak@hotmail.com>
2021-09-11 16:30:40 +0100

    added gender controller and views

---
+++ Controllers/GenderController.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VacationTracker.Areas.Identity.Data;
+using VacationTracker.Areas.Identity.Extensions;
+using VacationTracker.Data;
+using VacationTracker.Models;
+
+namespace VacationTracker.Controllers
+{
+    public class GenderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public GenderController(ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            int currentUsersCompanyId = User.Identity.GetCompanyId();
+            IEnumerable<Gender> genderList = _db.Genders.Where(x => x.CompanyId == currentUsersCompanyId && x.IsDeleted == false);
+            return View(genderList);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            int currentUsersCompanyId = User.Identity.GetCompanyId();
+
+            Gender gender = await _db.Genders.FirstOrDefaultAsync(x => x.Id == id && x.CompanyId == currentUsersCompanyId && x.IsDeleted == false);
+
+            if (gender == null)
+            {
+                return NotFound();
+            }
+            return View(gender);
+        }
+
+        // A boolean method to check if any genders exist
+        private bool GenderExists(int id)
+        {
+            return _db.Genders.Any(l => l.Id == id);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Gender());
+        }
+
+        // HttpPost method to create genders
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Gender gender)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(gender);
+            }
+
+            int currentUsersCompanyId = User.Identity.GetCompanyId();
+
+            gender.CompanyId = currentUsersCompanyId;
+
+            _db.Genders.Add(gender);
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenderExists(gender.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            int currentUsersCompanyId = User.Identity.GetCompanyId();
+
+            Gender gender = await _db.Genders.FirstOrDefaultAsync(x => x.Id == id && x.CompanyId == currentUsersCompanyId);
+
+            if (gender == null)
+            {
+                return NotFound();
+            }
+            return View(gender);
+        }
+
+        // HttpPost method to edit genders
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Gender gender)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(gender);
+            }
+
+            _db.Attach(gender).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenderExists(gender.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            int currentUsersCompanyId = User.Identity.GetCompanyId();
+
+            Gender gender = await _db.Genders.FirstOrDefaultAsync(x => x.Id == id && x.CompanyId == currentUsersCompanyId);
+
+            if (gender == null)
+            {
+                return NotFound();
+            }
+            return View(gender);
+        }
+
+        // HttpPost method to delete genders
+        [HttpPost]
+        public async Task<IActionResult> Delete(Gender gender)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(gender);
+            }
+
+            gender.IsDeleted = true;
+
+            _db.Attach(gender).State = EntityState.Modified;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenderExists(gender.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
