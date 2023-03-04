4a2b53af4d65a455b43ac14288c170bb0b308047
Bart <bart.rybak@hotmail.com>
2023-03-04 23:12:26 +0000

    update from .Net 5 to 6

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using VacationTracker.Models;
 
 namespace VacationTracker.Controllers
